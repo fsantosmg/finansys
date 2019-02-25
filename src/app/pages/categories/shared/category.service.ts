548ef0c2e2d752ccdba97bc71eb101b8fef301b8
Felipe Santos <fsantosmg@gmail.com>
2019-02-25 15:57:00 -0300

    listar categorias categorias-component

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -37,7 +37,7 @@ export class CategoryService {
   }
 
   update(category: Category): Observable<Category>{
-    const url = `${this.apiPath}/${id}`;
+    const url = `${this.apiPath}/${category.id}`;
 
     return this.http.put(url, category).pipe(
       catchError(this.handleError),
